6fdd07576bfb35a681e9258c90ab6db6f7776333
bharat <bharat.khiani@gmail.com>
2016-09-03 10:59:48 +0530

    Added validation for internal/external dtd
    Created the form and handled the scenario where the dtd is specified in
    the xml file either as inline definition or as a reference to an external
    dtd file. Yet to do checkbox integration for dtd that is not referenced in
    the xml.

---
+++ xmlvalidator1/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace xmlvalidator1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txbXmlFile = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.txbExternalDtdFile = new System.Windows.Forms.TextBox();
+            this.btnValidate = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txbResults = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 17);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Xml File:";
+            // 
+            // txbXmlFile
+            // 
+            this.txbXmlFile.Location = new System.Drawing.Point(94, 14);
+            this.txbXmlFile.Margin = new System.Windows.Forms.Padding(5);
+            this.txbXmlFile.Name = "txbXmlFile";
+            this.txbXmlFile.ReadOnly = true;
+            this.txbXmlFile.Size = new System.Drawing.Size(463, 26);
+            this.txbXmlFile.TabIndex = 1;
+            this.txbXmlFile.Text = "Click here to choose...";
+            this.txbXmlFile.Click += new System.EventHandler(this.txbXmlFile_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(15, 67);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(140, 24);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "External DTD:";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // txbExternalDtdFile
+            // 
+            this.txbExternalDtdFile.Enabled = false;
+            this.txbExternalDtdFile.Location = new System.Drawing.Point(153, 65);
+            this.txbExternalDtdFile.Margin = new System.Windows.Forms.Padding(5);
+            this.txbExternalDtdFile.Name = "txbExternalDtdFile";
+            this.txbExternalDtdFile.ReadOnly = true;
+            this.txbExternalDtdFile.Size = new System.Drawing.Size(404, 26);
+            this.txbExternalDtdFile.TabIndex = 4;
+            this.txbExternalDtdFile.Text = "Click here to choose...";
+            // 
+            // btnValidate
+            // 
+            this.btnValidate.Location = new System.Drawing.Point(12, 134);
+            this.btnValidate.Name = "btnValidate";
+            this.btnValidate.Size = new System.Drawing.Size(173, 44);
+            this.btnValidate.TabIndex = 5;
+            this.btnValidate.Text = "&Validate";
+            this.btnValidate.UseVisualStyleBackColor = true;
+            this.btnValidate.Click += new System.EventHandler(this.btnValidate_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 201);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Results: ";
+            // 
+            // txbResults
+            // 
+            this.txbResults.Location = new System.Drawing.Point(16, 226);
+            this.txbResults.Margin = new System.Windows.Forms.Padding(5);
+            this.txbResults.Multiline = true;
+            this.txbResults.Name = "txbResults";
+            this.txbResults.ReadOnly = true;
+            this.txbResults.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txbResults.Size = new System.Drawing.Size(541, 225);
+            this.txbResults.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(572, 454);
+            this.Controls.Add(this.txbResults);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnValidate);
+            this.Controls.Add(this.txbExternalDtdFile);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.txbXmlFile);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txbXmlFile;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox txbExternalDtdFile;
+        private System.Windows.Forms.Button btnValidate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txbResults;
+    }
+}
+
---
+++ xmlvalidator1/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+using System.Xml;
+
+namespace xmlvalidator1
+{
+    public partial class Form1 : Form
+    {
+        bool validationError = false;
+        string seperator = Environment.NewLine + "--------------------" + Environment.NewLine;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txbXmlFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd1 = new OpenFileDialog();
+            ofd1.InitialDirectory = Application.StartupPath;
+            ofd1.Filter = "XML Files (*.xml)|*.xml";
+            ofd1.ShowDialog();
+            
+            txbXmlFile.Text = ofd1.FileName;
+        }
+
+        void settings_ValidationEventHandler(object sender, System.Xml.Schema.ValidationEventArgs e)
+        {
+            validationError = true;
+            ShowResults(e.Message);
+        }
+
+        private void btnValidate_Click(object sender, EventArgs e)
+        {
+            validationError = false;
+            string filename = txbXmlFile.Text;
+
+            if (filename.Length == 0 || !File.Exists(filename))
+            {
+                txbResults.AppendText("Invalid filename/path" + seperator);
+                return;
+            }
+
+            // Check if it has DOCTYPE
+            string data = File.ReadAllText(filename);
+
+            if (data.IndexOf("<!DOCTYPE") != -1)
+            {
+                XmlReaderSettings settings = new XmlReaderSettings();
+                settings.ValidationType = ValidationType.DTD;
+                settings.DtdProcessing = DtdProcessing.Parse;
+                settings.ValidationEventHandler += settings_ValidationEventHandler;
+
+                XmlReader reader = XmlReader.Create(filename, settings);
+                while (reader.Read()) ; // traverse the xml file, validates dtd againg body.
+
+                reader.Close();
+                if(!validationError)
+                    ShowResults("Document is valid and meets DTD requirements");
+            }
+            else
+            {
+                ShowResults("No DOCTYPE present in xml, pls include it or check the checkbox to select the dtd");
+            }
+            
+
+           
+        }
+
+        private void ShowResults(string text)
+        {
+            txbResults.AppendText(text + seperator);
+        }
+    }
+}
